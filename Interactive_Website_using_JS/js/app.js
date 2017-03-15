f07a76c01a6f7582f4c9fe11202639e974fcb5a4
AdamBigos <AdamBigos@users.noreply.github.com>
2017-03-15 22:07:36 +0100

    Add files via upload

--- Interactive_Website_using_JS/js/app.js
+++ Interactive_Website_using_JS/js/app.js
@@ -1,35 +1,35 @@
 var main = function() {
     $('.top_wraper').click(function() {
-    ('.menu').show();
+    $('.menu').show();
    });
     
-    $('.icon_close').click(function() {
-    ('.menu').hide();
+    $('.article_title').click(function() {
+    $('.menu').hide();
    });
     
-  $('.title_row').click(function() {
-    $('.title_row').removeClass('current');
-    $('.description_row').hide();
+  //$('.article_title').click(function() {
+  //  $('.article_title').removeClass('current');
+  //  $('.description_row').hide();
 
-    $(this).addClass('current');
-    $(this).children('.description_row').show();
-  });
+  //  $(this).addClass('current');
+  //  $(this).children('.description_row').show();
+  //});
 
-  $(document).keypress(function(event) {
-    if(event.which === 111) {
-      $('.description_row').hide();
+ // $(document).keypress(function(event) {
+  //  if(event.which === 111) {
+ //     $('.description_row').hide();
 
-      $('.current').children('.description_row').show();
-    }
+ //     $('.current').children('.description_row').show();
+  //  }
 
-    else if(event.which === 110) {
-      var currentArticle = $('.current');
-      var nextArticle = currentArticle.next();
+  //  else if(event.which === 110) {
+  //    var currentArticle = $('.current');
+  //    var nextArticle = currentArticle.next();
       
-      currentArticle.removeClass('current');
-      nextArticle.addClass('current');
-    }
-  });
+ //     currentArticle.removeClass('current');
+ //     nextArticle.addClass('current');
+ //   }
+//  });
 }
 
-$(document).ready(main);
+$( window ).on( "load", main );
